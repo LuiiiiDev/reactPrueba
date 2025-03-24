9f06b8f029348bd8af55885a456e7e1474fd44bb
LuiiiDev <luisesc1210@gmail.com>
2025-03-24 14:42:48 -0600

    calc y todo function

---
+++ ejercicio-3/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Calc from './Components/Calc'
+import Todo from "./Components/To-do/Todo";
+
+
+function App() {
+  return (
+    <>
+      <Calc/>
+      <Todo/>
+    </>
+  );
+}
+
+export default App;
